39c9d4bb34ebd12dfa3270ca8ac6af0b972680f6
aaronyao <aaron.yao@oracle.com>
2019-05-13 18:44:42 +0800

    1.create layout according to the requirements

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,52 @@ import './App.css';
 function App() {
   return (
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Test</a>
-      </header>
+    <div className="main">
+       <div className="main-header">HEADER</div>
+
+       <div className="main-body">
+          <div className="main-left">left</div>
+          <div className="main-center">
+
+            <div className="item-breed">
+              <div className="dog-breed">
+                <select className="dog-breed-select">
+                  <option value ="volvo">Volvo</option>
+                  <option value ="saab">Saab</option>
+                  <option value="opel">Opel</option>
+                  <option value="audi">Audi</option>
+                </select>
+              </div>
+
+              <div className="dog-subbreed">
+                <select className="dog-breed-select">
+                  <option value ="volvo">Volvo</option>
+                  <option value ="saab">Saab</option>
+                  <option value="opel">Opel</option>
+                  <option value="audi">Audi</option>
+                </select>
+              </div>
+
+            </div>
+
+            <div className="item-images">
+              <div className="dog-image">
+                <img src="" alt="Dog Image" />
+              </div>
+
+              <div className="dog-image">
+                <img src="" alt="Dog Image" />
+              </div>
+
+            </div>
+          </div>
+
+          <div className="main-right">right</div>
+       </div>
+
+       <div className="main-footer">Bottom</div>
     </div>
+
   );
 }
 
